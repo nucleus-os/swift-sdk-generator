894daa27b34d487d4e6cd679a90525d101c1e081
Maddy <maddythewisp@gmail.com>
2026-08-23 18:57:58 -0600

    Add Ubuntu 26.04.1 to the Ubuntu distribution model

--- Sources/SwiftSDKGenerator/PlatformModels/LinuxDistribution.swift
+++ Sources/SwiftSDKGenerator/PlatformModels/LinuxDistribution.swift
@@ -11,224 +11,233 @@
 //===----------------------------------------------------------------------===//
 
 public enum LinuxDistribution: Hashable, Sendable {
-  public enum Name: String, Sendable {
-    case rhel
-    case ubuntu
-    case debian
-  }
+    public enum Name: String, Sendable {
+        case rhel
+        case ubuntu
+        case debian
+    }
 
-  public enum RHEL: String, Sendable {
-    case ubi9
-  }
+    public enum RHEL: String, Sendable {
+        case ubi9
+    }
 
-  public enum Ubuntu: String, Sendable, Equatable, CaseIterable {
-    case focal
-    case jammy
-    case noble
+    public enum Ubuntu: String, Sendable, Equatable, CaseIterable {
+        case focal
+        case jammy
+        case noble
+        case resolute
 
-    init(version: String) throws {
-      switch version {
-      case "20.04": self = .focal
-      case "22.04": self = .jammy
-      case "24.04": self = .noble
-      default:
-        guard let versionType = Self(rawValue: version) else {
-          throw GeneratorError.unknownLinuxDistribution(
-            name: LinuxDistribution.Name.ubuntu.rawValue,
-            version: version
-          )
+        init(version: String) throws {
+            switch version {
+            case "20.04": self = .focal
+            case "22.04": self = .jammy
+            case "24.04": self = .noble
+            case "26.04.1": self = .resolute
+            default:
+                guard let versionType = Self(rawValue: version) else {
+                    throw GeneratorError.unknownLinuxDistribution(
+                        name: LinuxDistribution.Name.ubuntu.rawValue,
+                        version: version
+                    )
+                }
+                self = versionType
+            }
         }
-        self = versionType
-      }
-    }
 
-    var version: String {
-      switch self {
-      case .focal: return "20.04"
-      case .jammy: return "22.04"
-      case .noble: return "24.04"
-      }
-    }
-
-    private var commonPackages: [String] {
-      [
-        "libc6",
-        "libc6-dev",
-        "libgcc-s1",
-        "libstdc++6",
-        "linux-libc-dev",
-        "zlib1g",
-        "zlib1g-dev",
-        "libicu-dev",
-        "libcurl4-openssl-dev",
-      ]
-    }
-
-    public var requiredPackages: [String] {
-      switch self {
-      case .focal:
-        return commonPackages + [
-          "libgcc-10-dev",
-          "libicu66",
-          "libstdc++-10-dev",
-        ]
-      case .jammy:
-        return commonPackages + [
-          "libgcc-12-dev",
-          "libicu70",
-          "libstdc++-12-dev",
-        ]
-      case .noble:
-        return commonPackages + [
-          "libgcc-13-dev",
-          "libicu74",
-          "libstdc++-13-dev",
-        ]
-      }
-    }
-  }
-
-  public enum Debian: String, Sendable, Equatable, CaseIterable {
-    case bullseye
-    case bookworm
-    case trixie
-
-    init(version: String) throws {
-      switch version {
-      case "11": self = .bullseye
-      case "12": self = .bookworm
-      case "13": self = .trixie
-      default:
-        guard let versionType = Self(rawValue: version) else {
-          throw GeneratorError.unknownLinuxDistribution(
-            name: LinuxDistribution.Name.debian.rawValue,
-            version: version
-          )
+        var version: String {
+            switch self {
+            case .focal: return "20.04"
+            case .jammy: return "22.04"
+            case .noble: return "24.04"
+            case .resolute: return "26.04.1"
+            }
         }
-        self = versionType
-      }
+
+        private var commonPackages: [String] {
+            [
+                "libc6",
+                "libc6-dev",
+                "libgcc-s1",
+                "libstdc++6",
+                "linux-libc-dev",
+                "zlib1g",
+                "zlib1g-dev",
+                "libicu-dev",
+                "libcurl4-openssl-dev",
+            ]
+        }
+
+        public var requiredPackages: [String] {
+            switch self {
+            case .focal:
+                return commonPackages + [
+                    "libgcc-10-dev",
+                    "libicu66",
+                    "libstdc++-10-dev",
+                ]
+            case .jammy:
+                return commonPackages + [
+                    "libgcc-12-dev",
+                    "libicu70",
+                    "libstdc++-12-dev",
+                ]
+            case .noble:
+                return commonPackages + [
+                    "libgcc-13-dev",
+                    "libicu74",
+                    "libstdc++-13-dev",
+                ]
+            case .resolute:
+                return commonPackages + [
+                    "libgcc-13-dev",
+                    "libicu78",
+                    "libstdc++-13-dev",
+                ]
+            }
+        }
     }
 
-    var version: String {
-      switch self {
-      case .bullseye: return "11"
-      case .bookworm: return "12"
-      case .trixie: return "13"
-      }
+    public enum Debian: String, Sendable, Equatable, CaseIterable {
+        case bullseye
+        case bookworm
+        case trixie
+
+        init(version: String) throws {
+            switch version {
+            case "11": self = .bullseye
+            case "12": self = .bookworm
+            case "13": self = .trixie
+            default:
+                guard let versionType = Self(rawValue: version) else {
+                    throw GeneratorError.unknownLinuxDistribution(
+                        name: LinuxDistribution.Name.debian.rawValue,
+                        version: version
+                    )
+                }
+                self = versionType
+            }
+        }
+
+        var version: String {
+            switch self {
+            case .bullseye: return "11"
+            case .bookworm: return "12"
+            case .trixie: return "13"
+            }
+        }
+
+        private var commonPackages: [String] {
+            [
+                "libc6",
+                "libc6-dev",
+                "libgcc-s1",
+                "libstdc++6",
+                "linux-libc-dev",
+                "zlib1g",
+                "zlib1g-dev",
+                "libicu-dev",
+                "libcurl4-openssl-dev",
+            ]
+        }
+
+        public var requiredPackages: [String] {
+            switch self {
+            case .bullseye:
+                return commonPackages + [
+                    "libgcc-10-dev",
+                    "libicu67",
+                    "libstdc++-10-dev",
+                ]
+            case .bookworm:
+                return commonPackages + [
+                    "libgcc-12-dev",
+                    "libicu72",
+                    "libstdc++-12-dev",
+                ]
+            case .trixie:
+                return commonPackages + [
+                    "libgcc-14-dev",
+                    "libicu76",
+                    "libstdc++-14-dev",
+                ]
+            }
+        }
     }
 
-    private var commonPackages: [String] {
-      [
-        "libc6",
-        "libc6-dev",
-        "libgcc-s1",
-        "libstdc++6",
-        "linux-libc-dev",
-        "zlib1g",
-        "zlib1g-dev",
-        "libicu-dev",
-        "libcurl4-openssl-dev",
-      ]
+    case rhel(RHEL)
+    case ubuntu(Ubuntu)
+    case debian(Debian)
+
+    public init(name: Name, version: String) throws {
+        switch name {
+        case .rhel:
+            guard let version = RHEL(rawValue: version) else {
+                throw GeneratorError.unknownLinuxDistribution(name: name.rawValue, version: version)
+            }
+            self = .rhel(version)
+
+        case .ubuntu:
+            self = try .ubuntu(Ubuntu(version: version))
+
+        case .debian:
+            self = try .debian(Debian(version: version))
+        }
     }
 
-    public var requiredPackages: [String] {
-      switch self {
-      case .bullseye:
-        return commonPackages + [
-          "libgcc-10-dev",
-          "libicu67",
-          "libstdc++-10-dev",
-        ]
-      case .bookworm:
-        return commonPackages + [
-          "libgcc-12-dev",
-          "libicu72",
-          "libstdc++-12-dev",
-        ]
-      case .trixie:
-        return commonPackages + [
-          "libgcc-14-dev",
-          "libicu76",
-          "libstdc++-14-dev",
-        ]
-      }
+    var name: Name {
+        switch self {
+        case .rhel: return .rhel
+        case .ubuntu: return .ubuntu
+        case .debian: return .debian
+        }
     }
-  }
 
-  case rhel(RHEL)
-  case ubuntu(Ubuntu)
-  case debian(Debian)
-
-  public init(name: Name, version: String) throws {
-    switch name {
-    case .rhel:
-      guard let version = RHEL(rawValue: version) else {
-        throw GeneratorError.unknownLinuxDistribution(name: name.rawValue, version: version)
-      }
-      self = .rhel(version)
-
-    case .ubuntu:
-      self = try .ubuntu(Ubuntu(version: version))
-
-    case .debian:
-      self = try .debian(Debian(version: version))
+    var release: String {
+        switch self {
+        case .rhel(let rhel): return rhel.rawValue
+        case .ubuntu(let ubuntu): return ubuntu.rawValue
+        case .debian(let debian): return debian.rawValue
+        }
     }
-  }
 
-  var name: Name {
-    switch self {
-    case .rhel: return .rhel
-    case .ubuntu: return .ubuntu
-    case .debian: return .debian
+    var swiftDockerImageSuffix: String {
+        switch self {
+        case .rhel(let rhel): return "rhel-\(rhel.rawValue)"
+        case .ubuntu(let ubuntu): return ubuntu.rawValue
+        case .debian(let debian): return debian.rawValue
+        }
     }
-  }
-
-  var release: String {
-    switch self {
-    case let .rhel(rhel): return rhel.rawValue
-    case let .ubuntu(ubuntu): return ubuntu.rawValue
-    case let .debian(debian): return debian.rawValue
-    }
-  }
-
-  var swiftDockerImageSuffix: String {
-    switch self {
-    case let .rhel(rhel): return "rhel-\(rhel.rawValue)"
-    case let .ubuntu(ubuntu): return ubuntu.rawValue
-    case let .debian(debian): return debian.rawValue
-    }
-  }
 }
 
 extension LinuxDistribution.Name {
-  public init(nameString: String) throws {
-    guard let name = LinuxDistribution.Name(rawValue: nameString) else {
-      throw GeneratorError.unknownLinuxDistribution(name: nameString, version: nil)
+    public init(nameString: String) throws {
+        guard let name = LinuxDistribution.Name(rawValue: nameString) else {
+            throw GeneratorError.unknownLinuxDistribution(name: nameString, version: nil)
+        }
+        self = name
     }
-    self = name
-  }
 }
 
 extension LinuxDistribution: CustomStringConvertible {
-  public var description: String {
-    let versionComponent: String
-    switch self {
-    case .rhel:
-      versionComponent = self.release.uppercased()
-    case .ubuntu, .debian:
-      versionComponent = self.release.capitalized
-    }
+    public var description: String {
+        let versionComponent: String
+        switch self {
+        case .rhel:
+            versionComponent = self.release.uppercased()
+        case .ubuntu, .debian:
+            versionComponent = self.release.capitalized
+        }
 
-    return "\(self.name) \(versionComponent)"
-  }
+        return "\(self.name) \(versionComponent)"
+    }
 }
 
 extension LinuxDistribution.Name: CustomStringConvertible {
-  public var description: String {
-    switch self {
-    case .rhel: return "RHEL"
-    case .ubuntu: return "Ubuntu"
-    case .debian: return "Debian"
+    public var description: String {
+        switch self {
+        case .rhel: return "RHEL"
+        case .ubuntu: return "Ubuntu"
+        case .debian: return "Debian"
+        }
     }
-  }
 }
--- Tests/SwiftSDKGeneratorTests/PlatformModels/LinuxDistributionTests.swift
+++ Tests/SwiftSDKGeneratorTests/PlatformModels/LinuxDistributionTests.swift
@@ -11,185 +11,198 @@
 //===----------------------------------------------------------------------===//
 
 #if canImport(Testing)
-  import Testing
-  @testable import SwiftSDKGenerator
+import Testing
+@testable import SwiftSDKGenerator
 
-  struct LinuxDistributionTests {
+struct LinuxDistributionTests {
     struct UbuntuTests {
-      @Test(arguments: [
-        ("20.04", LinuxDistribution.Ubuntu.focal),
-        ("focal", LinuxDistribution.Ubuntu.focal),
+        @Test(arguments: [
+            ("20.04", LinuxDistribution.Ubuntu.focal),
+            ("focal", LinuxDistribution.Ubuntu.focal),
 
-        ("22.04", LinuxDistribution.Ubuntu.jammy),
-        ("jammy", LinuxDistribution.Ubuntu.jammy),
+            ("22.04", LinuxDistribution.Ubuntu.jammy),
+            ("jammy", LinuxDistribution.Ubuntu.jammy),
 
-        ("24.04", LinuxDistribution.Ubuntu.noble),
-        ("noble", LinuxDistribution.Ubuntu.noble),
-      ])
-      func validVersionStrings(versionString: String, expectedVersion: LinuxDistribution.Ubuntu) throws {
-        let version = try LinuxDistribution.Ubuntu(version: versionString)
-        #expect(version == expectedVersion)
-      }
+            ("24.04", LinuxDistribution.Ubuntu.noble),
+            ("noble", LinuxDistribution.Ubuntu.noble),
 
-      @Test(arguments: [
-        "18.04",
-        "bionic",
-        "unknown",
-        "invalid",
-      ]) func invalidVersionStrings(versionString: String) throws {
-        #expect(throws: GeneratorError.self) {
-          let _ = try LinuxDistribution.Ubuntu(version: versionString)
+            ("26.04.1", LinuxDistribution.Ubuntu.resolute),
+            ("resolute", LinuxDistribution.Ubuntu.resolute),
+        ])
+        func validVersionStrings(versionString: String, expectedVersion: LinuxDistribution.Ubuntu)
+            throws
+        {
+            let version = try LinuxDistribution.Ubuntu(version: versionString)
+            #expect(version == expectedVersion)
         }
-      }
 
-      @Test(arguments: [
-        (LinuxDistribution.Ubuntu.focal, "20.04"),
-        (LinuxDistribution.Ubuntu.jammy, "22.04"),
-        (LinuxDistribution.Ubuntu.noble, "24.04"),
-      ])
-      func versionProperty(ubuntuVersion: LinuxDistribution.Ubuntu, expectedVersionString: String) {
-        #expect(ubuntuVersion.version == expectedVersionString)
-      }
+        @Test(arguments: [
+            "18.04",
+            "bionic",
+            "unknown",
+            "invalid",
+        ]) func invalidVersionStrings(versionString: String) throws {
+            #expect(throws: GeneratorError.self) {
+                let _ = try LinuxDistribution.Ubuntu(version: versionString)
+            }
+        }
 
-      @Test(arguments: LinuxDistribution.Ubuntu.allCases)
-      func requiredPackages(ubuntuVersion: LinuxDistribution.Ubuntu) {
-        let commonPackages = [
-          "libc6",
-          "libc6-dev",
-          "libgcc-s1",
-          "libstdc++6",
-          "linux-libc-dev",
-          "zlib1g",
-          "zlib1g-dev",
-          "libicu-dev",
-          "libcurl4-openssl-dev",
-        ]
+        @Test(arguments: [
+            (LinuxDistribution.Ubuntu.focal, "20.04"),
+            (LinuxDistribution.Ubuntu.jammy, "22.04"),
+            (LinuxDistribution.Ubuntu.noble, "24.04"),
+            (LinuxDistribution.Ubuntu.resolute, "26.04.1"),
+        ])
+        func versionProperty(ubuntuVersion: LinuxDistribution.Ubuntu, expectedVersionString: String)
+        {
+            #expect(ubuntuVersion.version == expectedVersionString)
+        }
 
-        let requiredPackages = ubuntuVersion.requiredPackages
-        #expect(requiredPackages.starts(with: commonPackages))
+        @Test(arguments: LinuxDistribution.Ubuntu.allCases)
+        func requiredPackages(ubuntuVersion: LinuxDistribution.Ubuntu) {
+            let commonPackages = [
+                "libc6",
+                "libc6-dev",
+                "libgcc-s1",
+                "libstdc++6",
+                "linux-libc-dev",
+                "zlib1g",
+                "zlib1g-dev",
+                "libicu-dev",
+                "libcurl4-openssl-dev",
+            ]
 
-        // Some required packages that change versions between Ubuntu versions
-        #expect(requiredPackages.contains(where: { $0.matches(regex: "libgcc-\\d{2}-dev") }))
-        #expect(requiredPackages.contains(where: { $0.matches(regex: "libicu\\d{2}") }))
-        #expect(requiredPackages.contains(where: { $0.matches(regex: "libstdc\\+\\+-\\d{2}-dev") }))
-      }
+            let requiredPackages = ubuntuVersion.requiredPackages
+            #expect(requiredPackages.starts(with: commonPackages))
+
+            // Some required packages that change versions between Ubuntu versions
+            #expect(requiredPackages.contains(where: { $0.matches(regex: "libgcc-\\d{2}-dev") }))
+            #expect(requiredPackages.contains(where: { $0.matches(regex: "libicu\\d{2}") }))
+            #expect(
+                requiredPackages.contains(where: { $0.matches(regex: "libstdc\\+\\+-\\d{2}-dev") }))
+        }
     }
 
     struct DebianTests {
-      @Test(arguments: [
-        ("11", LinuxDistribution.Debian.bullseye),
-        ("bullseye", LinuxDistribution.Debian.bullseye),
+        @Test(arguments: [
+            ("11", LinuxDistribution.Debian.bullseye),
+            ("bullseye", LinuxDistribution.Debian.bullseye),
 
-        ("12", LinuxDistribution.Debian.bookworm),
-        ("bookworm", LinuxDistribution.Debian.bookworm),
+            ("12", LinuxDistribution.Debian.bookworm),
+            ("bookworm", LinuxDistribution.Debian.bookworm),
 
-        ("13", LinuxDistribution.Debian.trixie),
-        ("trixie", LinuxDistribution.Debian.trixie),
-      ])
-      func validVersionStrings(versionString: String, expectedVersion: LinuxDistribution.Debian) throws {
-        let version = try LinuxDistribution.Debian(version: versionString)
-        #expect(version == expectedVersion)
-      }
-
-      @Test(arguments: [
-        "9",
-        "sid",
-        "unknown",
-        "invalid",
-      ]) func invalidVersionStrings(versionString: String) throws {
-        #expect(throws: GeneratorError.self) {
-          let _ = try LinuxDistribution.Debian(version: versionString)
+            ("13", LinuxDistribution.Debian.trixie),
+            ("trixie", LinuxDistribution.Debian.trixie),
+        ])
+        func validVersionStrings(versionString: String, expectedVersion: LinuxDistribution.Debian)
+            throws
+        {
+            let version = try LinuxDistribution.Debian(version: versionString)
+            #expect(version == expectedVersion)
         }
-      }
 
-      @Test(arguments: [
-        (LinuxDistribution.Debian.bullseye, "11"),
-        (LinuxDistribution.Debian.bookworm, "12"),
-        (LinuxDistribution.Debian.trixie, "13"),
-      ])
-      func versionProperty(debianVersion: LinuxDistribution.Debian, expectedVersionString: String) {
-        #expect(debianVersion.version == expectedVersionString)
-      }
+        @Test(arguments: [
+            "9",
+            "sid",
+            "unknown",
+            "invalid",
+        ]) func invalidVersionStrings(versionString: String) throws {
+            #expect(throws: GeneratorError.self) {
+                let _ = try LinuxDistribution.Debian(version: versionString)
+            }
+        }
 
-      @Test(arguments: LinuxDistribution.Debian.allCases)
-      func requiredPackages(debianVersion: LinuxDistribution.Debian) {
-        let commonPackages = [
-          "libc6",
-          "libc6-dev",
-          "libgcc-s1",
-          "libstdc++6",
-          "linux-libc-dev",
-          "zlib1g",
-          "zlib1g-dev",
-          "libicu-dev",
-          "libcurl4-openssl-dev",
-        ]
+        @Test(arguments: [
+            (LinuxDistribution.Debian.bullseye, "11"),
+            (LinuxDistribution.Debian.bookworm, "12"),
+            (LinuxDistribution.Debian.trixie, "13"),
+        ])
+        func versionProperty(debianVersion: LinuxDistribution.Debian, expectedVersionString: String)
+        {
+            #expect(debianVersion.version == expectedVersionString)
+        }
 
-        let requiredPackages = debianVersion.requiredPackages
-        #expect(requiredPackages.starts(with: commonPackages))
+        @Test(arguments: LinuxDistribution.Debian.allCases)
+        func requiredPackages(debianVersion: LinuxDistribution.Debian) {
+            let commonPackages = [
+                "libc6",
+                "libc6-dev",
+                "libgcc-s1",
+                "libstdc++6",
+                "linux-libc-dev",
+                "zlib1g",
+                "zlib1g-dev",
+                "libicu-dev",
+                "libcurl4-openssl-dev",
+            ]
 
-        // Some required packages that change versions between Debian versions
-        #expect(requiredPackages.contains(where: { $0.matches(regex: "libgcc-\\d{2}-dev") }))
-        #expect(requiredPackages.contains(where: { $0.matches(regex: "libicu\\d{2}") }))
-        #expect(requiredPackages.contains(where: { $0.matches(regex: "libstdc\\+\\+-\\d{2}-dev") }))
-      }
+            let requiredPackages = debianVersion.requiredPackages
+            #expect(requiredPackages.starts(with: commonPackages))
+
+            // Some required packages that change versions between Debian versions
+            #expect(requiredPackages.contains(where: { $0.matches(regex: "libgcc-\\d{2}-dev") }))
+            #expect(requiredPackages.contains(where: { $0.matches(regex: "libicu\\d{2}") }))
+            #expect(
+                requiredPackages.contains(where: { $0.matches(regex: "libstdc\\+\\+-\\d{2}-dev") }))
+        }
     }
 
     @Test(arguments: [
-      (LinuxDistribution.Name.rhel, "ubi9", "ubi9", "rhel-ubi9"),
-      (LinuxDistribution.Name.ubuntu, "22.04", "jammy", "jammy"),
-      (LinuxDistribution.Name.debian, "12", "bookworm", "bookworm"),
+        (LinuxDistribution.Name.rhel, "ubi9", "ubi9", "rhel-ubi9"),
+        (LinuxDistribution.Name.ubuntu, "22.04", "jammy", "jammy"),
+        (LinuxDistribution.Name.debian, "12", "bookworm", "bookworm"),
     ])
     func distributionProperties(
-      name: LinuxDistribution.Name,
-      version: String,
-      expectedRelease: String,
-      expectedImageSuffix: String
+        name: LinuxDistribution.Name,
+        version: String,
+        expectedRelease: String,
+        expectedImageSuffix: String
     ) throws {
-      let distribution = try LinuxDistribution(name: name, version: version)
+        let distribution = try LinuxDistribution(name: name, version: version)
 
-      #expect(distribution.name == name)
-      #expect(distribution.release == expectedRelease)
-      #expect(distribution.swiftDockerImageSuffix == expectedImageSuffix)
-      #expect(distribution.description.isEmpty == false)
+        #expect(distribution.name == name)
+        #expect(distribution.release == expectedRelease)
+        #expect(distribution.swiftDockerImageSuffix == expectedImageSuffix)
+        #expect(distribution.description.isEmpty == false)
     }
 
     @Test(arguments: [
-      ("rhel", LinuxDistribution.Name.rhel),
-      ("ubuntu", LinuxDistribution.Name.ubuntu),
-      ("debian", LinuxDistribution.Name.debian),
-    ]) func validDistributionNames(nameString: String, expectedName: LinuxDistribution.Name) throws {
-      let name = try LinuxDistribution.Name(nameString: nameString)
-      #expect(name == expectedName)
+        ("rhel", LinuxDistribution.Name.rhel),
+        ("ubuntu", LinuxDistribution.Name.ubuntu),
+        ("debian", LinuxDistribution.Name.debian),
+    ]) func validDistributionNames(nameString: String, expectedName: LinuxDistribution.Name) throws
+    {
+        let name = try LinuxDistribution.Name(nameString: nameString)
+        #expect(name == expectedName)
     }
 
     @Test(arguments: [
-      "amazonlinux",
-      "fedora",
-      "opensuse",
+        "amazonlinux",
+        "fedora",
+        "opensuse",
     ]) func invalidDistributionNames(name: String) throws {
-      #expect(throws: GeneratorError.self) {
-        let _ = try LinuxDistribution.Name(nameString: name)
-      }
+        #expect(throws: GeneratorError.self) {
+            let _ = try LinuxDistribution.Name(nameString: name)
+        }
     }
 
     @Test(arguments: [
-      (LinuxDistribution.Name.rhel, "ubi8"),
-      (LinuxDistribution.Name.ubuntu, "18.04"),
-      (LinuxDistribution.Name.debian, "9"),
+        (LinuxDistribution.Name.rhel, "ubi8"),
+        (LinuxDistribution.Name.ubuntu, "18.04"),
+        (LinuxDistribution.Name.debian, "9"),
     ]) func invalidDistributionVersions(
-      name: LinuxDistribution.Name,
-      version: String
+        name: LinuxDistribution.Name,
+        version: String
     ) throws {
-      #expect(throws: GeneratorError.self) {
-        let _ = try LinuxDistribution(name: name, version: version)
-      }
+        #expect(throws: GeneratorError.self) {
+            let _ = try LinuxDistribution(name: name, version: version)
+        }
     }
-  }
+}
 
-  extension String {
+extension String {
     func matches(regex: String) -> Bool {
-      return self.range(of: regex, options: .regularExpression) != nil
+        return self.range(of: regex, options: .regularExpression) != nil
     }
-  }
+}
 #endif
